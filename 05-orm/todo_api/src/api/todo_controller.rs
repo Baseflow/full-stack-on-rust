8d34db40d0cec864fb105d79ab6235882b86ddf3
Thomas Luijken <tluijken@outlook.com>
2022-09-26 14:22:31 +0200

    Added some sanity checks on the id paths

--- 05-orm/todo_api/src/api/todo_controller.rs
+++ 05-orm/todo_api/src/api/todo_controller.rs
@@ -10,11 +10,10 @@ use actix_web::web::Data;
 use std::sync::Arc;
 use uuid::Uuid;
 
-use log::{error, info, warn};
+use log::{error, warn};
 
 #[get("/todo")]
 async fn get_todos(repository: Data<dyn Repository<TodoEntity>>) -> impl Responder {
-    info!("Getting all todo items");
     // Get entities from the datastore
     let entities = repository.get_all();
 
@@ -30,26 +29,29 @@ async fn get_todo_by_id(
     id: web::Path<String>, // The identifier of the item to retrieve
     repository: Data<dyn Repository<TodoEntity>>, // The todo item repository, injected from app_data
 ) -> impl Responder {
-    let todo_id =
-        Uuid::parse_str(&id.into_inner()).expect("The given identifier is not a valid uuid");
+    let todo_id = Uuid::parse_str(&id.into_inner());
 
-    // Query our entity from the data store.
-    info!("Getting todo item with id {}", todo_id);
-    let entity = repository.get_by_id(todo_id);
-    match entity {
-        Some(item) => {
-            // If we found one, use the From<T> trait to convert to a TodoItem
-            let response: TodoItem = item.into();
-            // Send the response
-            HttpResponse::Ok().json(response)
+    match todo_id {
+        Ok(uuid) => {
+            // Query our entity from the data store.
+            let entity = repository.get_by_id(uuid);
+            match entity {
+                Some(item) => {
+                    // If we found one, use the From<T> trait to convert to a TodoItem
+                    let response: TodoItem = item.into();
+                    // Send the response
+                    HttpResponse::Ok().json(response)
+                }
+                _ => {
+                    warn!("Todo item with id {} was not found in the data store", uuid);
+                    // Let the caller know the resource was not found.
+                    HttpResponse::NotFound().finish()
+                }
+            }
         }
         _ => {
-            warn!(
-                "Todo item with id {} was not found in the data store",
-                todo_id
-            );
-            // Let the caller know the resource was not found.
-            HttpResponse::NotFound().finish()
+            warn!("User supplied an unvalid uuid");
+            HttpResponse::BadRequest().finish()
         }
     }
 }
@@ -78,17 +80,24 @@ async fn delete_todo(
     id: web::Path<String>,
     repository: Data<dyn Repository<TodoEntity>>, // The todo item repository, injected from app_data
 ) -> impl Responder {
-    let todo_id =
-        Uuid::parse_str(&id.into_inner()).expect("The given identifier is not a valid uuid");
-    let result = repository.delete(todo_id);
-    match result {
-        Ok(success) => match success {
-            true => HttpResponse::Ok().finish(),
-            _ => HttpResponse::NotFound().finish(),
-        },
+    let todo_id = Uuid::parse_str(&id.into_inner());
+    match todo_id {
+        Ok(uuid) => {
+            let result = repository.delete(uuid);
+            match result {
+                Ok(success) => match success {
+                    true => HttpResponse::Ok().finish(),
+                    _ => HttpResponse::NotFound().finish(),
+                },
+                _ => {
+                    error!("Unable to delete item");
+                    HttpResponse::InternalServerError().finish()
+                }
+            }
+        }
         _ => {
-            error!("Unable to delete item");
-            HttpResponse::InternalServerError().finish()
+            warn!("User supplied an unvalid uuid");
+            HttpResponse::BadRequest().finish()
         }
     }
 }
@@ -100,17 +109,25 @@ async fn update_todo(
     repository: Data<dyn Repository<TodoEntity>>, // The todo item repository, injected from app_data
 ) -> impl Responder {
     let request_body = todo.into_inner();
-    let todo_id =
-        Uuid::parse_str(&id.into_inner()).expect("The given identifier is not a valid uuid");
-    let result = repository.update(todo_id, request_body.into());
-    match result {
-        Ok(entity) => {
-            let result: TodoItem = entity.into();
-            HttpResponse::Ok().json(result)
+
+    let todo_id = Uuid::parse_str(&id.into_inner());
+    match todo_id {
+        Ok(uuid) => {
+            let result = repository.update(uuid, request_body.into());
+            match result {
+                Ok(entity) => {
+                    let result: TodoItem = entity.into();
+                    HttpResponse::Ok().json(result)
+                }
+                _ => {
+                    error!("Unable to update existing todo item");
+                    HttpResponse::InternalServerError().finish()
+                }
+            }
         }
         _ => {
-            error!("Unable to update existing todo item");
-            HttpResponse::InternalServerError().finish()
+            warn!("User supplied an unvalid uuid");
+            HttpResponse::BadRequest().finish()
         }
     }
 }
